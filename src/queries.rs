3f21e9fe748272d67b5289482a5c4999124aeac6
Jacob Goodier <jgoodier21@gmail.com>
2021-09-28 12:47:03 -0400

    saving the list as a file works properly

--- src/queries.rs
+++ src/queries.rs
@@ -1,6 +1,6 @@
+use std::cmp::PartialEq;
 use std::fs;
 use std::io::prelude::*;
-use std::cmp::PartialEq;
 
 use serde::{Deserialize, Serialize};
 
@@ -11,16 +11,16 @@ struct UserIdResp {
     data: UpdateUser,
 }
 
-#[allow(non_snake_case)]
 #[derive(Deserialize, Debug)]
+#[serde(rename_all(deserialize = "PascalCase"))]
 struct UpdateUser {
-    UpdateUser: UserData,
+    update_user: UserData,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct UserData {
     pub id: u32,
-    pub name: String
+    pub name: String,
 }
 
 #[derive(Deserialize, Debug)]
@@ -28,10 +28,10 @@ struct ListResp {
     data: MediaListCollection,
 }
 
-#[allow(non_snake_case)]
 #[derive(Deserialize, Debug)]
+#[serde(rename_all(deserialize = "PascalCase"))]
 struct MediaListCollection {
-    MediaListCollection: Lists,
+    media_list_collection: Lists,
 }
 
 #[derive(Deserialize, Debug)]
@@ -44,8 +44,7 @@ struct Entries {
     entries: Vec<Entry>,
 }
 
-#[allow(non_snake_case)]
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 struct Entry {
     status: MediaListStatus,
     score: f32,
@@ -54,50 +53,52 @@ struct Entry {
 }
 
 #[derive(Deserialize, Serialize, Debug, PartialEq, Clone)]
+#[serde(rename_all(deserialize = "SCREAMING_SNAKE_CASE", serialize = "PascalCase"))]
 enum MediaListStatus {
-    COMPLETED,
-    CURRENT,
-    PLANNING,
-    DROPPED,
-    PAUSED,
-    REPEATING,
+    Completed,
+    Current,
+    Planning,
+    Dropped,
+    Paused,
+    Repeating,
 }
 
-#[allow(non_snake_case)]
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all(deserialize = "camelCase", serialize = "snake_case"))]
 struct Media {
     id: u32,
-    idMal: u32,
+    id_mal: u32,
     title: Title,
     format: MediaFormat,
     episodes: u32,
 }
 
-#[allow(non_snake_case)]
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all(deserialize = "camelCase", serialize = "snake_case"))]
 struct Title {
-    userPreferred: String,
+    user_preferred: String,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
+#[serde(rename_all(deserialize = "SCREAMING_SNAKE_CASE", serialize = "PascalCase"))]
 enum MediaType {
-    ANIME,
-    MANGA,
+    Anime,
+    Manga,
 }
 
-#[allow(non_camel_case_types)]
-#[derive(Deserialize, Serialize, Debug, Clone)]
+#[derive(Deserialize, Serialize, Debug, Clone, Copy)]
+#[serde(rename_all(deserialize = "SCREAMING_SNAKE_CASE", serialize = "PascalCase"))]
 enum MediaFormat {
-    TV,
-    TV_SHORT,
-    MOVIE,
-    SPECIAL,
-    OVA,
-    ONA,
-    MUSIC,
-    MANGA,
-    NOVEL,
-    ONE_SHOT
+    Tv,
+    TvShort,
+    Movie,
+    Special,
+    Ova,
+    Ona,
+    Music,
+    Manga,
+    Novel,
+    OneShot,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -105,96 +106,92 @@ struct UserSection {
     user_id: u32,
     username: String,
     list_type: MediaType,
-    total_anime: usize,
-    watching: usize,
-    completed: usize,
-    on_hold: usize,
-    dropped: usize,
-    planning: usize,
-    rewatching: usize
+    total_anime: u32,
+    watching: u32,
+    completed: u32,
+    on_hold: u32,
+    dropped: u32,
+    planning: u32,
+    rewatching: u32,
 }
 
 impl UserSection {
     fn new(lists: &Lists, user: UserData) -> UserSection {
-        let watching_position = lists.lists.iter().position(|x| {
-            x.entries[0].status == MediaListStatus::CURRENT
-        });
+        let watching_position = lists
+            .lists
+            .iter()
+            .position(|x| x.entries[0].status == MediaListStatus::Current);
         let watching_len = match watching_position {
             None => 0,
-            Some(watching_position) => {
-                lists.lists[watching_position].entries.len()
-            }
+            Some(watching_position) => lists.lists[watching_position].entries.len() as u32,
         };
 
-        let completed_position = lists.lists.iter().position(|x| {
-            x.entries[0].status == MediaListStatus::COMPLETED
-        });
+        let completed_position = lists
+            .lists
+            .iter()
+            .position(|x| x.entries[0].status == MediaListStatus::Completed);
         let completed_len = match completed_position {
             None => 0,
-            Some(completed_position) => {
-                lists.lists[completed_position].entries.len()
-            }
+            Some(completed_position) => lists.lists[completed_position].entries.len() as u32,
         };
 
-        let paused_position = lists.lists.iter().position(|x| {
-            x.entries[0].status == MediaListStatus::PAUSED
-        });
+        let paused_position = lists
+            .lists
+            .iter()
+            .position(|x| x.entries[0].status == MediaListStatus::Paused);
         let paused_len = match paused_position {
             None => 0,
-            Some(paused_position) => {
-                lists.lists[paused_position].entries.len()
-            }
+            Some(paused_position) => lists.lists[paused_position].entries.len() as u32,
         };
 
-        let dropped_position = lists.lists.iter().position(|x| {
-            x.entries[0].status == MediaListStatus::DROPPED
-        });
+        let dropped_position = lists
+            .lists
+            .iter()
+            .position(|x| x.entries[0].status == MediaListStatus::Dropped);
         let dropped_len = match dropped_position {
             None => 0,
-            Some(dropped_position) => {
-                lists.lists[dropped_position].entries.len()
-            }
+            Some(dropped_position) => lists.lists[dropped_position].entries.len() as u32,
         };
 
-        let planning_position = lists.lists.iter().position(|x| {
-            x.entries[0].status == MediaListStatus::PLANNING
-        });
+        let planning_position = lists
+            .lists
+            .iter()
+            .position(|x| x.entries[0].status == MediaListStatus::Planning);
         let planning_len = match planning_position {
             None => 0,
-            Some(planning_position) => {
-                lists.lists[planning_position].entries.len()
-            }
+            Some(planning_position) => lists.lists[planning_position].entries.len() as u32,
         };
 
-        let repeating_position = lists.lists.iter().position(|x| {
-            x.entries[0].status == MediaListStatus::REPEATING
-        });
+        let repeating_position = lists
+            .lists
+            .iter()
+            .position(|x| x.entries[0].status == MediaListStatus::Repeating);
         let repeating_len = match repeating_position {
             None => 0,
-            Some(repeating_position) => {
-                lists.lists[repeating_position].entries.len()
-            }
+            Some(repeating_position) => lists.lists[repeating_position].entries.len() as u32,
         };
 
-        let total = watching_len + completed_len + paused_len + dropped_len + planning_len + repeating_len;
+        let total =
+            watching_len + completed_len + paused_len + dropped_len + planning_len + repeating_len;
 
         UserSection {
             user_id: user.id,
             username: user.name,
-            list_type: MediaType::ANIME,
+            list_type: MediaType::Anime,
             total_anime: total,
             watching: watching_len,
             completed: completed_len,
             on_hold: paused_len,
             dropped: dropped_len,
             planning: planning_len,
-            rewatching: repeating_len
+            rewatching: repeating_len,
         }
     }
 }
 
 #[derive(Deserialize, Serialize, Debug)]
 struct EntrySection {
+    title: String,
     id: u32,
     id_mal: u32,
     episodes: u32,
@@ -206,9 +203,12 @@ struct EntrySection {
 
 impl EntrySection {
     fn new(entry: &Entry) -> EntrySection {
+        let title = entry.media.title.user_preferred.clone().replace(" ", "-");
+
         EntrySection {
+            title,
             id: entry.media.id,
-            id_mal: entry.media.idMal,
+            id_mal: entry.media.id_mal,
             episodes: entry.media.episodes,
             format: entry.media.format.clone(),
             status: entry.status.clone(),
@@ -216,6 +216,35 @@ impl EntrySection {
             progress: entry.progress,
         }
     }
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Current(Vec<EntrySection>);
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Completed(Vec<EntrySection>);
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Planning(Vec<EntrySection>);
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Dropped(Vec<EntrySection>);
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Paused(Vec<EntrySection>);
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Repeating(Vec<EntrySection>);
+
+#[derive(Deserialize, Serialize, Debug)]
+struct BackupToml {
+    user_section: UserSection,
+    repeating: Repeating,
+    current: Current,
+    completed: Completed,
+    paused: Paused,
+    dropped: Dropped,
+    planning: Planning,
 }
 
 const GET_USER_ID: &str = "
@@ -226,8 +255,6 @@ mutation {
     }
 }
 ";
-
-
 
 pub async fn get_user_id(access_token: String) -> UserData {
     let auth_header = format!("Bearer {}", access_token);
@@ -245,11 +272,12 @@ pub async fn get_user_id(access_token: String) -> UserData {
         .await
         .unwrap()
         .text()
-        .await;
+        .await
+        .unwrap();
 
-    let result: UserIdResp = serde_json::from_str(&res.unwrap()).unwrap();
-    println!("{}", result.data.UpdateUser.id);
-    result.data.UpdateUser
+    let result: UserIdResp = serde_json::from_str(&res).unwrap();
+    println!("{}", result.data.update_user.id);
+    result.data.update_user
 }
 
 const GET_LIST: &str = "
@@ -291,46 +319,59 @@ pub async fn get_list(config: &Config) -> Lists {
         .await
         .unwrap()
         .text()
-        .await;
+        .await
+        .unwrap();
 
-    let result: ListResp = serde_json::from_str(&res.unwrap()).unwrap();
-    result.data.MediaListCollection
+    let result: ListResp = serde_json::from_str(&res).unwrap();
+    result.data.media_list_collection
 }
 
 pub fn write_list_to_file(list: &Lists, user: (u32, &str)) {
     let user = UserData {
         id: user.0,
-        name: user.1.to_string()
+        name: user.1.to_string(),
     };
     // create the file
-    let mut file = fs::OpenOptions::new()
+    let file = fs::OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
         .open("anilist-backup.toml")
         .unwrap();
 
-    // add the user section
     let user_section = UserSection::new(list, user);
-    // println!("{:?}", user_section);
-    let user_section_toml = toml::to_string(&user_section).unwrap();
-    writeln!(&file, "[User Information]").unwrap();
-    write!(&file, "{} \n", user_section_toml).unwrap();
 
-    // loop through the list and its entries and write them to the
-    // want to sort them by their status first
-    for list in list.lists.iter() {
-        // want to sort them by their title first
-        for entry in list.entries.iter() {
-            let entry_section = EntrySection::new(&entry);
-            write_media(&mut file, entry_section, &entry.media.title.userPreferred).unwrap();
-        }
-    }
+    let current_list = Current(create_entry_section_vec(&list, MediaListStatus::Current));
+    let completed_list = Completed(create_entry_section_vec(&list, MediaListStatus::Completed));
+    let planning_list = Planning(create_entry_section_vec(&list, MediaListStatus::Planning));
+    let dropped_list = Dropped(create_entry_section_vec(&list, MediaListStatus::Dropped));
+    let paused_list = Paused(create_entry_section_vec(&list, MediaListStatus::Paused));
+    let repeating_list = Repeating(create_entry_section_vec(&list, MediaListStatus::Repeating));
+    let backup = BackupToml {
+        user_section,
+        current: current_list,
+        repeating: repeating_list,
+        completed: completed_list,
+        paused: paused_list,
+        dropped: dropped_list,
+        planning: planning_list,
+    };
+    let backup_toml = toml::to_string(&backup).unwrap();
+    writeln!(&file, "{}", backup_toml).unwrap();
 }
 
-fn write_media(file: &mut std::fs::File, entry: EntrySection, title: &str) -> std::io::Result<()>{
-    let entry_toml = toml::to_string(&entry).unwrap();
-    writeln!(file, "[{}]", title)?;
-    write!(file, "{} \n", entry_toml)?;
-    Ok(())
+fn create_entry_section_vec(list: &Lists, status: MediaListStatus) -> Vec<EntrySection> {
+    let list_pos = list
+        .lists
+        .iter()
+        .position(|x| x.entries[0].status == status)
+        .unwrap();
+    let list = &list.lists[list_pos].entries;
+
+    let mut vec = Vec::new();
+    for entry in list.iter() {
+        let entry_section = EntrySection::new(&entry);
+        vec.push(entry_section);
+    }
+    vec
 }
